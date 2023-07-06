3dae9a98f543ce0de5d328f1deb79126713525f5
iamuput <yogaaasp73@gmail.com>
2023-07-07 00:45:20 +0700

    Update help.py

--- Uputt/modules/user/help.py
+++ Uputt/modules/user/help.py
@@ -44,7 +44,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @Ammuyeee •",
+                f"```{str(ac)}```\n• @gbtniki •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Ammuyeee"
+            this_command += "© @gbtniki"
             #hehehehehe
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
@@ -86,7 +86,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @Ammuyeee •"
+            message, f"```{str(ac)}```\n• @gbtniki •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -98,7 +98,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Ammuyeee"
+            this_command += "© @gbtniki"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
