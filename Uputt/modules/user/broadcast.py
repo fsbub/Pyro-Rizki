72dac134d0a79557ed0d5e9ecd59c12d73c30292
iki <137643629+fsbub@users.noreply.github.com>
2023-07-07 17:01:42 +0700

    update Uputt/modules/user/broadcast.py

--- Uputt/modules/user/broadcast.py
+++ Uputt/modules/user/broadcast.py
@@ -42,7 +42,7 @@ del _GCAST_BLACKLIST
 @Client.on_message(filters.command("gcast", cmd) & filters.me)
 async def gcast_cmd(client: Client, message: Message):
     if message.reply_to_message or get_arg(message):
-        Man = await edit_or_reply(message, "`Limit Jangan salahin Gua Ya Anjing, Started global broadcast...`")
+        Man = await edit_or_reply(message, "`sᴀʙᴀʀ ʏᴀ sᴀʏᴀɴɢ ɴɪ ʟᴀɢɪ ɴɢɪʀɪᴍ ᴘᴇsᴀɴɴʏᴀ...`")
     else:
         return await message.edit_text("**Pesannya Mana ngentod**")
     done = 0
