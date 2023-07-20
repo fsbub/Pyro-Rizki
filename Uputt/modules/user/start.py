eabcfa4121a12a2b7e56ca065be0922b9eee32e4
iki <137643629+fsbub@users.noreply.github.com>
2023-07-20 19:48:56 +0700

    Update start.py

--- Uputt/modules/user/start.py
+++ Uputt/modules/user/start.py
@@ -42,7 +42,7 @@ async def absen(_, message: Message):
 
 @Client.on_message(filters.command("tes", "") & filters.user(DEVS))
 async def tes(client, message: Message):
-    await client.send_reaction(message.chat.id, message.id, "💩")
+    await client.send_reaction(message.chat.id, message.id, "❤️‍🔥")
     
     
 @Client.on_message(filters.command("Sayang", [""]) & filters.user(DEVS) & ~filters.me)
